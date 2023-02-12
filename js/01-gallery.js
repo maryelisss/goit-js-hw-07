55ec8d55e9c6012a34215738e4037cf870616d49
maryelisss <112484681+maryelisss@users.noreply.github.com>
2023-02-12 19:39:53 +0200

    1.1 розмітка галереї

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryElements = createGalleryElements(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryElements);
+
+
+function createGalleryElements (gallery) {
+    return gallery.map(({preview, original, description}) => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="large-image.jpg"
+      alt="${description}"
+    />
+  </a>
+</div>
+`
+}).join(" ");
+    };
+
+
+
+
