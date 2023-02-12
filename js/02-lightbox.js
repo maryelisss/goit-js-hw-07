4eba8f6b86e0336bcdd48d953626a3bae194e491
maryelisss <112484681+maryelisss@users.noreply.github.com>
2023-02-13 00:10:10 +0200

    2.1 start

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryElements = createGalleryElements(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryElements);
+
+galleryContainer.addEventListener('click', galleryOpen);
+
+function createGalleryElements (gallery) {
+    return gallery.map(({preview, original, description}) => {
+        return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" 
+        alt="${description}" />
+      </a>
+`
+}).join(" ");
+    };
+
+function galleryOpen () {};    
